4c563c9843515c6ddee5534f06465eedc35a398b
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-12-22 12:14:46 +0200

    Adjust LXC dnsmasq part

--- provision.sh
+++ provision.sh
@@ -9,9 +9,6 @@ sudo dpkg-reconfigure debconf -f noninteractive -p critical
 sudo apt-get update >/dev/null
 sudo apt-get install -y lxc vim mc htop ansible>/dev/null
 
-# This has to be installed separately due to: https://github.com/lxc/lxd/issues/543
-sudo apt-get install -y dnsmasq>/dev/null
-
 [ -f /home/vagrant/.bash_aliases ] || sudo -u vagrant cp /vagrant/bash_aliases /home/vagrant/.bash_aliases
 [ -f /home/vagrant/.bash_functions ] || sudo -u vagrant cp /vagrant/bash_functions /home/vagrant/.bash_functions
 [ -f /home/vagrant/.vagrant_aliases ] || sudo -u vagrant cp /vagrant/vagrant_aliases /home/vagrant/.vagrant_aliases
@@ -28,3 +25,7 @@ sudo apt-get install -y dnsmasq>/dev/null
 [ -d /home/vagrant/.vagrant.d/gems/gems/vagrant-triggers-0.5.3 ] || sudo -u vagrant tar zxf /vagrant/gems.tar.gz -C /home/vagrant/.vagrant.d/ || HOME=/home/vagrant sudo -u vagrant vagrant plugin install vagrant-triggers --plugin-version=0.5.3
 [ -f /home/vagrant/.vagrant.d/plugins.json ] || sudo -u vagrant cp /vagrant/plugins.json /home/vagrant/.vagrant.d/plugins.json
 [ -d /home/vagrant/.vagrant.d/boxes/oxid_esales-VAGRANTSLASH-trusty64-lxc/0/lxc ] || HOME=/home/vagrant sudo -u vagrant vagrant box add /vagrant/lxc.box --name=oxid_esales/trusty64-lxc
+
+sudo patch /etc/default/lxc /vagrant/default.lxc.patch
+sudo cp /vagrant/lxc-dnsmasq.conf /etc/lxc/dnsmasq.conf
+sudo service lxc-net restart
